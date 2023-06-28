84bf82402ab824c09fa368cb0faa185a3da3c5f3
lekksha_ <lekksha@gmail.com>
2023-06-28 18:37:30 +0700

    tracker functions check
    for some reason there is no filename output to console

--- main.cpp
+++ main.cpp
@@ -8,5 +8,16 @@
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+    FileTracker test_tracker = FileTracker();
+    test_tracker.addFile("C:/Users/lekks/Desktop/Labs3_2/TRPO/FileTracker_testfiles/1.txt");
+    test_tracker.addFile("C:/Users/lekks/Desktop/Labs3_2/TRPO/FileTracker_testfiles/1.txt");
+
+    test_tracker.removeFile("C:/Users/lekks/Desktop/Labs3_2/TRPO/FileTracker_testfiles/1.txt");
+    test_tracker.removeFile("C:/Users/lekks/Desktop/Labs3_2/TRPO/FileTracker_testfiles/1.txt");
+
+    //QTimer *timer = new QTimer(this);
+    //connect(timer, &QTimer::timeout, this, &Foo::processOneThing);
+с
+
     return a.exec();
 }
