f4b2081f30fd63b76160a1522440a4d845922059
lekksha_ <lekksha@gmail.com>
2023-08-13 19:57:00 +0900

    Working prototype
    signal-slot output in a separate class is currently WIP

--- main.cpp
+++ main.cpp
@@ -12,19 +12,15 @@ int main(int argc, char *argv[])
     FileTracker test_tracker = FileTracker();
     test_tracker.addFile("C:/Users/lekks/Desktop/Labs3_2/TRPO/FileTracker_testfiles/1.txt");
     test_tracker.addFile("C:/Users/lekks/Desktop/Labs3_2/TRPO/FileTracker_testfiles/1.txt");
-
     test_tracker.removeFile("C:/Users/lekks/Desktop/Labs3_2/TRPO/FileTracker_testfiles/1.txt");
     test_tracker.removeFile("C:/Users/lekks/Desktop/Labs3_2/TRPO/FileTracker_testfiles/1.txt");
 
-    //QTimer *timer = new QTimer(this);
-    //connect(timer, &QTimer::timeout, this, &Foo::processOneThing);
+    test_tracker.addFile("C:/Users/lekks/Desktop/Labs3_2/TRPO/FileTracker_testfiles/1.txt");
 
-    /*
     QTimer timer;
     timer.setInterval(500);
     QObject::connect(&timer, &QTimer::timeout, &test_tracker, &FileTracker::update);
     timer.start();
-    */
 
     return a.exec();
 
