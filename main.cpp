462fb9c77c12bce1beac70f50a23488eaa4d78cc
lekksha_ <lekksha@gmail.com>
2023-08-27 16:04:14 +0900

    Minor improvements

--- main.cpp
+++ main.cpp
@@ -11,12 +11,17 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
     FileTracker* test_tracker = FileTracker::getInstance();
     Output* test_output = Output::getInstance(*test_tracker);
+
+
+    // The files need to be added below
     test_tracker->addFile("C:/Users/lekks/TSU/semester 6/TRPO/FileTracker_testfiles/1.txt");
     test_tracker->addFile("C:/Users/lekks/TSU/semester 6/TRPO/FileTracker_testfiles/1.txt");
     test_tracker->removeFile("C:/Users/lekks/TSU/semester 6/TRPO/FileTracker_testfiles/1.txt");
     test_tracker->removeFile("C:/Users/lekks/TSU/semester 6/TRPO/FileTracker_testfiles/1.txt");
 
     test_tracker->addFile("C:/Users/lekks/TSU/semester 6/TRPO/FileTracker_testfiles/1.txt");
+    //
+
 
     QTimer timer;
     timer.setInterval(500);
