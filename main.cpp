cb24f5c5348be4eb6ec03a25cd22f56cbd58c3d6
lekksha_ <lekksha@gmail.com>
2024-04-23 15:00:17 +0700

    Comments update

--- main.cpp
+++ main.cpp
@@ -8,8 +8,8 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     FileTracker* test_tracker = FileTracker::getInstance();
-    Output* test_output = Output::getInstance(*test_tracker);
-
+    [[maybe_unused]] Output* test_output = Output::getInstance(*test_tracker);
+    // attribute is used to suppress warning. variable is essential for functioning despite being "unused" as compiler says
 
     // The files need to be added below
     test_tracker->addFile("C:/Users/lekks/TSU/semester 6/TRPO/FileTracker_testfiles/1.txt");
@@ -20,7 +20,6 @@ int main(int argc, char *argv[])
     test_tracker->addFile("C:/Users/lekks/TSU/semester 6/TRPO/FileTracker_testfiles/1.txt");
     test_tracker->addFile("C:/Users/lekks/TSU/semester 6/TRPO/FileTracker_testfiles/2.txt");
     //
-
 
     QTimer timer;
     timer.setInterval(500);
